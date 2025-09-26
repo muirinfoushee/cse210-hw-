f220c19ace7f45bfc33a061903cde9634a9c6dec
muirinfoushee <fou23001@byui.edu>
2025-09-25 22:00:34 -0600

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a series of numbers (type 0 to stop):");
+
+        do
+        {
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+
+            }
+        } while (input != 0);
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = 0;
+        if (numbers.Count > 0)
+        {
+            average = (double)sum / numbers.Count;
+        }
+        Console.WriteLine($"The average is: {average}");
+
+
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
